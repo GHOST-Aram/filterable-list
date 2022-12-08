cda3ac7896337d645483bdc83b006ed6e2230a50
GHOST-Aram <owinoojala@gmail.com>
2022-12-08 11:21:30 +0300

    Change color for filtered text

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function filterContacts(input){
-    const contacts = document.querySelectorAll('p')
-    
+    const contacts = document.querySelectorAll('p')    
     const regex = new RegExp(input,'ig')
+
     contacts.forEach(contact => {
-        if(regex.test(contact.textContent))
+        if(regex.test(contact.textContent)){
             contact.style.display = 'block'
-        else
+            contact.style.color = 'maroon'
+        }
+        else{
+            contact.style.color = 'grey'
             contact.style.display = 'none'
+        }
+        if(input.trim()==='')
+            contact.style.color = 'grey'
+
     });
 }
 
-const contactGroup = document.querySelectorAll('.contact-group')
 const searchBox = document.querySelector('input')
 
-searchBox.addEventListener('keyup', (event)=>{
+searchBox.addEventListener('input', (event)=>{
     filterContacts(searchBox.value)
-    console.log(event)
 })
